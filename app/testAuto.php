14154c3d091ab18d3ed05559004d2e7cc1734524
LautiLezama <lautarolezama@hotmail.com>
2021-03-23 21:49:33 -0300

    Ejercicio 17

--- app/testAuto.php
+++ app/testAuto.php
@@ -1,4 +1,7 @@
 <?php
+
+include "classAuto.php";
+
 //Misma marca - Distinto color
 $auto1 = new Auto("Renault", "Amarillo");
 $auto2 = new Auto("Renault", "Rojo");
@@ -7,6 +10,7 @@ $auto3 = new Auto("Chevrolet","Azul",120000);
 $auto4 = new Auto("Chevrolet","Azul",400000);
 //Ultima sobrecarga
 $auto5 = new Auto("Honda", "Negro", 300000, date('d-m-Y'));
+var_dump($auto5);
 
 $auto3->AgregarImpuestos(1500);
 $auto4->AgregarImpuestos(1500);
@@ -17,89 +21,30 @@ $sumaDeAutos = Auto::Add($auto1, $auto2);
 print("El primer auto y el segundo : ");
 if($auto1->Equals($auto2))
 {
-    print("Son iguales");
+    print("Son iguales<br>");
 }
 else
 {
-    print("No son iguales");
+    print("No son iguales<br>");
 }
 
 print("El primer auto y el quinto : ");
 if($auto1->Equals($auto5))
 {
-    print("Son iguales");
+    print("Son iguales<br>");
 }
 else
 {
-    print("No son iguales");
+    print("No son iguales<br>");
 }
 
+print("<br><br>Auto 1: <br>");
 print(Auto::MostrarAuto($auto1));
+print("<br><br>Auto 3: <br>");
 print(Auto::MostrarAuto($auto3));
+print("<br><br>Auto 5: <br>");
 print(Auto::MostrarAuto($auto5));
 
-class Auto
-{
-    private $_color;
-    private $_precio;
-    private $_marca;
-    private $_fecha;
 
-    public function __constructor($marca, $color)
-    {
-        $this->_marca = $marca;
-        $this->_color = $color;
-    }
-
-    public function __constructor2($marca, $color, $precio)
-    {
-        $this->_marca = $marca;
-        $this->_color = $color;
-        $this->_precio = $precio;
-    }
-
-    public function __constructor3($marca, $color, $precio, $fecha)
-    {
-        $this->_marca = $marca;
-        $this->_color = $color;
-        $this->_precio = $precio;
-        $this->_fecha = $fecha;
-    }
-
-    public function AgregarImpuestos($impuesto)
-    {
-        $this->_precio = $this->_precio + $impuesto;
-    }
-
-    public static function MostrarAuto($auto)
-    {
-        return "Marca: $auto->_precio <br>Color: $auto->_color <br>Precio: $auto->_precio <br>Fecha: $auto->_fecha";
-    }
-
-    public function Equals($auto2)
-    {
-        if($this->_marca == $auto2->_marca)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public static function Add($auto1, $auto2)
-    {
-        if($auto1->_marca == $auto2->_marca && $auto1->_color == $auto2->_color)
-        {
-            return $auto1->_precio + $auto2->_precio;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    
-}
 
 ?>
