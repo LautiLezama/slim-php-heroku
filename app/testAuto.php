118ef78331f87b1f95f85ff37c4a2a6be6fd24a8
LautiLezama <lautarolezama@hotmail.com>
2021-03-23 21:50:45 -0300

    ejercicio 17 terminado

--- app/testAuto.php
+++ app/testAuto.php
@@ -10,7 +10,7 @@ $auto3 = new Auto("Chevrolet","Azul",120000);
 $auto4 = new Auto("Chevrolet","Azul",400000);
 //Ultima sobrecarga
 $auto5 = new Auto("Honda", "Negro", 300000, date('d-m-Y'));
-var_dump($auto5);
+
 
 $auto3->AgregarImpuestos(1500);
 $auto4->AgregarImpuestos(1500);
