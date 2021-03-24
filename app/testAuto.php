da806ab73fa299598a56b8dae24b1ddd45100b86
LautiLezama <lautarolezama@hotmail.com>
2021-03-23 21:06:55 -0300

    testAuto

--- app/testAuto.php
+++ app/testAuto.php
@@ -1,10 +1,42 @@
 <?php
+//Misma marca - Distinto color
+$auto1 = new Auto("Renault", "Amarillo");
+$auto2 = new Auto("Renault", "Rojo");
+//Misma marca - Mismo precio - Distinto precio
+$auto3 = new Auto("Chevrolet","Azul",120000);
+$auto4 = new Auto("Chevrolet","Azul",400000);
+//Ultima sobrecarga
+$auto5 = new Auto("Honda", "Negro", 300000, date('d-m-Y'));
 
-$auto1 = new Auto();
+$auto3->AgregarImpuestos(1500);
+$auto4->AgregarImpuestos(1500);
+$auto5->AgregarImpuestos(1500);
 
+$sumaDeAutos = Auto::Add($auto1, $auto2);
 
+print("El primer auto y el segundo : ");
+if($auto1->Equals($auto2))
+{
+    print("Son iguales");
+}
+else
+{
+    print("No son iguales");
+}
 
+print("El primer auto y el quinto : ");
+if($auto1->Equals($auto5))
+{
+    print("Son iguales");
+}
+else
+{
+    print("No son iguales");
+}
 
+print(Auto::MostrarAuto($auto1));
+print(Auto::MostrarAuto($auto3));
+print(Auto::MostrarAuto($auto5));
 
 class Auto
 {
