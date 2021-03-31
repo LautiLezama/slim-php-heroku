7300c1c5158d2061e0acd7f16f8a089d276832f3
LautiLezama <lautarolezama@hotmail.com>
2021-03-30 21:45:39 -0300

    Ej 20

--- app/ejercicios-clase03/registro.php
+++ app/ejercicios-clase03/registro.php
@@ -2,7 +2,7 @@
 
 include "usuario.php";
 $nuevoUsuario = new Usuario();
-$nuevoUsuario->_usuario = $_POST["usuario"];
+$nuevoUsuario->_usuario = $_POST["nombre"];
 $nuevoUsuario->_clave = $_POST["clave"];
 $nuevoUsuario->_mail = $_POST["mail"];
 echo Usuario::_validarUsuario($nuevoUsuario);
